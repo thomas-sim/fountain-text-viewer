03709aed01e51db3a8fe3fdb31b4669b8f0fbc26
Tamica <tamica@disroot.org>
2020-09-15 14:08:06 +0200

    First release :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import fountain from './fountain.js'
 import './App.css';
 
+function FountainViewer ( props ) {
+    let fountainScript = props.fountainScript;
+    let backEvent = props.backEvent;
+    let character = props.character;
+
+    // parse the text
+    let html = fountain.parse(fountainScript, true).html;
+    let script = html.script;
+    let titlePage = html.title_page;
+
+    // if necessary, highlight character
+    if (character) {
+        const re = new RegExp('<div class="dialogue"><h4>' + character + '</h4>', 'g');
+        script = script.replace(re,
+            '<div class="dialogue highlighted"><h4>' + character + '</h4>');
+    }
+
+
+    return (
+        <div className="script">
+        <form className="no-print" onSubmit={backEvent}><input type="submit" value="Go back !"/> </form>
+        <div dangerouslySetInnerHTML={{ __html: html.title_page }}></div>
+        <div dangerouslySetInnerHTML={{ __html: script }}></div>
+        </div>
+    );
+}
+
+
+function useInput(initialValue){
+   const [value,setValue] = useState(initialValue);
+
+    function handleChange(e){
+        setValue(e.target.value);
+    }
+
+   return [value,handleChange];
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [fountainScript, setFountainScript] = useInput('');
+    const [character, setCharacter] = useInput('');
+
+    const [isText, setIsText] = useState(false);
+    const onSubmit = event => {
+        event.preventDefault();
+        setIsText(true);
+    }
+
+    const back = event => {
+        event.preventDefault()
+        setIsText(false);
+    }
+
+    let main;
+    if (isText) {
+        main = <FountainViewer fountainScript={fountainScript}
+                               character={character}
+                               backEvent={back}/>
+    } else {
+        main = (
+            <form onSubmit={onSubmit}>
+            <label>Write or Paste fountain script:<br/>
+                <textarea cols="90" rows="15" value={fountainScript} onChange={setFountainScript}/>
+            </label>
+            <label>Enter character name to be highlighted (optionnal):<br/>
+                <input type="text" name="character" value={character} onChange={setCharacter}/>
+            </label>
+            <br/>
+            <input type="submit" value="Parse the text" />
+            </form>
+        );
+    }
+
+    return (
+        <div className="App">
+        <header className="App-header">
+        {main}
+        </header>
+        </div>
+    );
 }
 
 export default App;
